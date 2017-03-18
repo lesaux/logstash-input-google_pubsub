c8ac1854aed98c1de49c5a51467a22162704ffd1
Pierig Le Saux <lesaux@pythian.com>
2017-03-18 17:16:19 -0400

    Update google_pubsub.rb
    typo

--- lib/logstash/inputs/google_pubsub.rb
+++ lib/logstash/inputs/google_pubsub.rb
@@ -16,7 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-equire "logstash/inputs/threadable"
+require "logstash/inputs/threadable"
 require 'logstash/namespace'
 
 # Google deps
